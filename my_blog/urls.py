f0a3dea6143062e364ba437f024f78d129c45bdb
tokhtaraubakirov <toxaaubakirovv2002@gmail.com>
2023-11-24 23:14:41 +0600

    feat: divided app into different modules and handled to render html pages

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("articles/", include("articles.urls")),
     path("", views.homepage, name="index"),
     path("about/", views.about, name="about"),
 ]
